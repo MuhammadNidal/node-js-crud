9401f3d1c1b5a7c42716c7d10bd4f625a14a6ecf
Muhammad Nidal <mnidal414@gmail.com>
2024-09-22 11:42:11 +0500

    put method

--- index.js
+++ index.js
@@ -33,6 +33,23 @@ const server = http.createServer((req, res) => {
             res.end(JSON.stringify({ message: 'Data added successfully', data: parsedData }));
         });
     }
+    
+    // Handle PUT /data (update existing data by id)
+    else if (url === '/put' && method === 'PUT') {
+        getRequestBody(req, (body) => {
+            const parsedData = JSON.parse(body);  // Parse the data to update
+            const index = dataStore.findIndex(item => item.id === parsedData.id); // Find the item by ID
+
+            if (index !== -1) {
+                dataStore[index] = parsedData;    // Update the data
+                res.writeHead(200);
+                res.end(JSON.stringify({ message: 'Data updated successfully', data: parsedData }));
+            } else {
+                res.writeHead(404);               // Send 404 if the item was not found
+                res.end(JSON.stringify({ message: 'Data not found' }));
+            }
+        });
+    }
        else {
         res.writeHead(404);
         res.end(JSON.stringify({ message: 'Route not found' }));
